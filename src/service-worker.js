9166feea1cbdc254e1cabb318e04b8f7111e0154
Alek Evans <gpg@alekeagle.com>
2022-02-23 12:57:52 -0700

    lol

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,4 @@
 const URLSToCache = [
-  'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap',
   '/service-worker.js',
   '/manifest.json',
   ...self.__precacheManifest.map(a => a.url)
@@ -100,7 +99,9 @@ self.addEventListener('fetch', async e => {
                 !url.host.startsWith('previews')) ||
               (url.pathname === '/' && url.host.startsWith('previews'))
             ) {
-              console.debug(`[CacheManager] ${url} is not eligible for caching`);
+              console.debug(
+                `[CacheManager] ${url} is not eligible for caching`
+              );
               return response;
             } else {
               newlyAdded = true;
