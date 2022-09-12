3ea41a7546e8ba762a2f35bf7497e201e8596f46
Alek Evans <alek@lugetron.com>
2022-09-12 10:07:36 -0800

    fix minor bug with paginator

--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -23,11 +23,11 @@
     </button>
   </div>
   <slot>
-    <h1> Some idiot forgot to put something in the slot </h1>
+    <h1>Some idiot forgot to put something in the slot</h1>
 
-    <h2> This is a paginator component. </h2>
+    <h2>This is a paginator component.</h2>
 
-    <p> We're on page {{ props.modelValue + 1 }} of {{ props.max + 1 }} </p>
+    <p>We're on page {{ props.modelValue + 1 }} of {{ props.max + 1 }}</p>
   </slot>
 
   <div class="paginator-controls paginator-controls-bottom">
@@ -57,99 +57,113 @@
 </template>
 
 <script lang="ts" setup>
-  import { onMounted, watch } from 'vue';
-  import { useRouter } from 'vue-router';
+import { onMounted, watch } from "vue";
+import { useRouter } from "vue-router";
 
-  const emit = defineEmits(['update:modelValue', 'pageChange']),
-    props = defineProps({
-      modelValue: {
-        type: Number,
-        required: true
-      },
-      min: {
-        type: Number,
-        default: 0
-      },
-      max: {
-        type: Number,
-        default: 0
-      },
-      disabled: Boolean
-    }),
-    router = useRouter();
+const emit = defineEmits(["update:modelValue", "pageChange"]),
+  props = defineProps({
+    modelValue: {
+      type: Number,
+      required: true,
+    },
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 0,
+    },
+    disabled: Boolean,
+  }),
+  router = useRouter();
 
-  function prevPage() {
-    emit('update:modelValue', props.modelValue - 1);
-    emit('pageChange');
+function prevPage() {
+  emit("update:modelValue", props.modelValue - 1);
+  emit("pageChange");
+}
+
+function nextPage() {
+  emit("update:modelValue", props.modelValue + 1);
+  emit("pageChange");
+}
+
+function onInputChange(event: Event) {
+  const input = event.target as HTMLInputElement;
+  if (input.value === "") {
+    input.value = props.modelValue + 1 + "";
+    return;
   }
-
-  function nextPage() {
-    emit('update:modelValue', props.modelValue + 1);
-    emit('pageChange');
+  if (Number(input.value) <= props.min) {
+    emit("update:modelValue", props.min);
+  } else if (Number(input.value) >= props.max) {
+    emit("update:modelValue", props.max);
+  } else {
+    emit("update:modelValue", Number(input.value) - 1);
   }
+  input.blur();
+  emit("pageChange");
+}
 
-  function onInputChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.value === '') {
-      input.value = props.modelValue + 1 + '';
+watch(props, ({ modelValue }) => {
+  router.replace({
+    query: {
+      ...router.currentRoute.value.query,
+      page: modelValue === 0 ? undefined : modelValue + 1,
+    },
+  });
+});
+
+onMounted(() => {
+  if (router.currentRoute.value.query.page) {
+    if (Number(router.currentRoute.value.query.page) === 1) {
+      router.replace({
+        query: {
+          ...router.currentRoute.value.query,
+          page: undefined,
+        },
+      });
       return;
     }
-    if (Number(input.value) <= props.min) {
-      emit('update:modelValue', props.min);
-    } else if (Number(input.value) >= props.max) {
-      emit('update:modelValue', props.max);
-    } else {
-      emit('update:modelValue', Number(input.value) - 1);
+    if (Number(router.currentRoute.value.query.page) > props.max + 1) {
+      router.replace({
+        query: {
+          ...router.currentRoute.value.query,
+          page: props.max + 1 + "",
+        },
+      });
     }
-    input.blur();
-    emit('pageChange');
+    emit("update:modelValue", Number(router.currentRoute.value.query.page) - 1);
   }
+});
 
-  watch(props, ({ modelValue }) => {
-    router.replace({
-      query: {
-        ...router.currentRoute.value.query,
-        page: modelValue === 0 ? undefined : modelValue + 1
-      }
-    });
-  });
-
-  onMounted(() => {
-    if (router.currentRoute.value.query.page) {
-      emit(
-        'update:modelValue',
-        Number(router.currentRoute.value.query.page) - 1
-      );
-    }
-  });
-
-  function validateInput(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.value === '') return;
-    if (Number(input.value) < props.min + 1) {
-      input.value = props.min + 1 + '';
-    } else if (Number(input.value) > props.max + 1) {
-      input.value = props.max + 1 + '';
-    }
+function validateInput(event: Event) {
+  const input = event.target as HTMLInputElement;
+  if (input.value === "") return;
+  if (Number(input.value) < props.min + 1) {
+    input.value = props.min + 1 + "";
+  } else if (Number(input.value) > props.max + 1) {
+    input.value = props.max + 1 + "";
   }
+}
 </script>
 
 <style>
-  .paginator-controls {
-    width: fit-content;
-    margin: 0 auto;
-  }
+.paginator-controls {
+  width: fit-content;
+  margin: 0 auto;
+}
 
-  .paginator-controls input {
-    width: 6ch;
-    margin: 0 0.5ch;
-  }
+.paginator-controls input {
+  width: 6ch;
+  margin: 0 0.5ch;
+}
 
-  .paginator-controls.paginator-controls-top {
-    margin-bottom: 5px;
-  }
+.paginator-controls.paginator-controls-top {
+  margin-bottom: 5px;
+}
 
-  .paginator-controls.paginator-controls-bottom {
-    margin-top: 5px;
-  }
+.paginator-controls.paginator-controls-bottom {
+  margin-top: 5px;
+}
 </style>
