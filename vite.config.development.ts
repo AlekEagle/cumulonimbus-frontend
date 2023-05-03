e03d397fda63aed67b8aebcf6612a94709d35d8d
Alek Evans <alek@lugetron.com>
2023-05-03 09:49:20 -0800

    Test legacy support

--- vite.config.development.ts
+++ vite.config.development.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -100,6 +101,7 @@ export default defineConfig({
         enabled: true
       }
     }),
-    vue()
+    vue(),
+    legacy()
   ]
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -93,6 +94,7 @@ export default defineConfig({
         enabled: true
       }
     }),
-    vue()
+    vue(),
+    legacy()
   ]
 });
