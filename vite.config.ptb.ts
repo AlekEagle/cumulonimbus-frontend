919904f2b5af60799df35cd462c08279902f011a
Alek Evans <github@alekeagle.com>
2024-01-13 11:23:49 -0900

    Remove legacy bundles
    Legacy bundles target such a small subset and doesn't include the service worker

--- vite.config.ptb.ts
+++ vite.config.ptb.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
-import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -116,6 +115,5 @@ export default defineConfig({
       },
     }),
     vue(),
-    legacy(),
   ],
 });
